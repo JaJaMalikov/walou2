f3ae9bc447164dab3606dbf6191ae3d342cb8d5c
JaJaMalikov <jerome@chivet.net>
2025-10-02 23:06:07 +0200

    qq

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    base: process.env.REPO_NAME ? `/${process.env.REPO_NAME}/` : '/',
+
     return {
       server: {
         port: 3000,
@@ -11,10 +13,6 @@ export default defineConfig(({ mode }) => {
       },
       publicDir: "assets",
       plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
