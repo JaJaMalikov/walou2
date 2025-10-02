bb5e99c738ff34de55a2defd0bcf38159bab1fcd
JaJaMalikov <jerome@chivet.net>
2025-10-02 23:14:52 +0200

    jj

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    base: process.env.REPO_NAME ? `/${process.env.REPO_NAME}/` : '/',
-
     return {
+      base: process.env.REPO_NAME ? `/${process.env.REPO_NAME}/` : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
